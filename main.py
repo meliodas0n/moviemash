62f517bef95fa3354b59de2d8716f0b6eed0473f
meliodas0n <mrunalnarayana@gmail.com>
2024-04-30 19:52:51 +0530

    fastapi initiated

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask
+from typing import Union
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-if __name__ == "__main__":
-    app.run(debug = True)
+@app.get("/")
+def read_root():
+  return {"Hello" : "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+  return {"item_id" : item_id, "q" : q}
